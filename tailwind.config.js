845958bbf5b8bb250ae3b2033bf2d972b4220b42
coderkishan-web <shindekishan937@gmail.com>
2024-08-22 11:31:27 +0530

    tailwind CLI has been added by kishan

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html","blogs.html","about.html","contact.html","courses.html","jobportal.html","registration.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
